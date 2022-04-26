ff176ea649552b832dc9ed74ea06209de51d2595
Ribin Roy <ribinroy4@gmail.com>
2022-04-26 13:43:18 +0530

    lazy loaded

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,28 @@
-import { useContext, useEffect } from 'react';
-import Main from './Sections/Main';
-import MintYourOwn from './Sections/MintYourOwn';
-import Slider from './Sections/Slider';
+import React, { useContext, useEffect, Suspense } from 'react';
+// import Main from './Sections/Main';
+// import MintYourOwn from './Sections/MintYourOwn';
+// import Slider from './Sections/Slider';
 import Footer from './Sections/Footer';
 import NewHome from './Sections/NewHome';
-import NFTs from './Sections/NFTs';
+// import NFTs from './Sections/NFTs';
 import './styles.scss';
 
 import VersusContext from '../../store/Context';
 
 import LoaderImage from '../../assets/images/Pattern.png';
-import AboutVersus from './Sections/AboutVersus';
-import AboutCyphers from './Sections/AboutCyphers';
-import Metaverse from './Sections/Metaverse';
-import BlockChain from './Sections/BlockChain';
+// import AboutVersus from './Sections/AboutVersus';
+// import AboutCyphers from './Sections/AboutCyphers';
+// import Metaverse from './Sections/Metaverse';
+// import BlockChain from './Sections/BlockChain';
+
+const BlockChain = React.lazy(() => import('./Sections/BlockChain'));
+const Metaverse = React.lazy(() => import('./Sections/Metaverse'));
+const AboutCyphers = React.lazy(() => import('./Sections/AboutCyphers'));
+const AboutVersus = React.lazy(() => import('./Sections/AboutVersus'));
+const NFTs = React.lazy(() => import('./Sections/NFTs'));
+const Slider = React.lazy(() => import('./Sections/Slider'));
+const MintYourOwn = React.lazy(() => import('./Sections/MintYourOwn'));
+const Main = React.lazy(() => import('./Sections/Main'));
 
 const Home = () => {
   const contextData = useContext(VersusContext);
@@ -32,14 +41,19 @@ const Home = () => {
       {
         <section className='home'>
           <NewHome />
-          <Main />
-          <NFTs />
-          <Slider />
-          <AboutVersus />
-          <AboutCyphers />
-          <BlockChain />
-          <Metaverse />
-          <MintYourOwn />
+          <Suspense fallback={''}>
+            <>
+              <Main />
+              <NFTs />
+              <Slider />
+              <AboutVersus />
+              <AboutCyphers />
+              <BlockChain />
+              <Metaverse />
+              <MintYourOwn />
+            </>
+          </Suspense>
+
           {/* <Footer /> */}
         </section>
       }
