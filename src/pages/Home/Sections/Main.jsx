c9f4e021d9cabde46b34d1f54d33d34af6777d40
Ribin Roy <ribinroy4@gmail.com>
2022-06-05 01:09:32 +0530

    discord link added

--- src/pages/Home/Sections/Main.jsx
+++ src/pages/Home/Sections/Main.jsx
@@ -11,57 +11,6 @@ import { ReactComponent as MainSmallButton } from '../../../assets/svg/Home/Main
 const Main = () => {
   return (
     <div className='main_section'>
-      {/* <img
-                src={GraphicLine}
-                alt='Graphic'
-                className='graph_line graphic_line1'
-            /> */}
-      {/* <img
-                src={GraphicLine}
-                alt='Graphic'
-                className='graph_line graphic_line2'
-            /> */}
-      {/* <img
-                src={GraphicLine}
-                alt='Graphic'
-                className='graph_line graphic_line3'
-            /> */}
-      {/* <img
-                src={GraphicLine}
-                alt='Graphic'
-                className='graph_line graphic_line4'
-            /> */}
-      {/* <img
-                src={GraphicLine1}
-                alt='Graphic'
-                className='graph_line graphic_line5'
-            /> */}
-      {/* <img
-                src={GraphicLine1}
-                alt='Graphic'
-                className='graph_line graphic_line6'
-            /> */}
-      {/* <img
-                src={GraphicLine1}
-                alt='Graphic'
-                className='graph_line graphic_line7'
-            /> */}
-      {/* <img
-                src={GraphicLine1}
-                alt='Graphic'
-                className='graph_line graphic_line8'
-            />
-            <img
-                src={GraphicLine1}
-                alt='Graphic'
-                className='graph_line graphic_line9'
-            />
-            <img
-                src={GraphicLine}
-                alt='Graphic'
-                className='graph_line graphic_line10'
-            /> */}
-
       <div className='content_wrap'>
         <div className='flex_box'>
           <div className='x1 image_wrap'>
@@ -75,33 +24,16 @@ const Main = () => {
             />
           </div>
           <div className='x2'>
-            
-            {/* <h1
-                            data-aos='fade-up'
-                            data-aos-offset='0'
-                            data-aos-duration='500'>
-                            A New World To Explore
-                        </h1>
-                        <p
-                            data-aos='fade-up'
-                            data-aos-offset='0'
-                            data-aos-duration='600'>
-                            Cyphers have come to life through the power of blockchain. 
-                            Mint your own and help them harness their power for epic rewards!
-                        </p>
-                        <button
-                            data-aos='fade-up'
-                            data-aos-offset='0'
-                            data-aos-duration='700'>
-                            Read the Docs
-                        </button> */}
             <div
               className='box_svg main_box'
               data-aos='fade-up'
               data-aos-offset='0'
               data-aos-duration='400'>
               <img src={MainBGImage} className='box_svg mobile_hidden' />
-              <div className="box_content">An Animal Crossing inspired open world multiplayer Metaverse game home to Cyphers, Trainers, and new cultures.</div>
+              <div className='box_content'>
+                An Animal Crossing inspired open world multiplayer Metaverse
+                game home to Cyphers, Trainers, and new cultures.
+              </div>
               <MainBG className='box_svg mobile_only' />
             </div>
             <div
@@ -110,8 +42,12 @@ const Main = () => {
               data-aos-offset='0'
               data-aos-duration='500'>
               <button className='transparent svg_wrap blue_button'>
-                <span></span>
-                <MainSmallButton />
+                <a
+                  href='https://discord.gg/QacH8SJGaw'
+                  target={'_blank'}
+                  rel='noreferrer'>
+                  <MainSmallButton />
+                </a>
               </button>
               <button className='transparent svg_wrap green_button'>
                 <span></span>
